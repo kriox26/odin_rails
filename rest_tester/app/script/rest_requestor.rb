21deaf90a3cf60130e8445f13c04846e833b788f
kriox26 <matias.pan26@gmail.com>
2015-07-10 23:45:52 -0300

    done with first part of basic_rvc project

---
+++ rest_tester/app/script/rest_requestor.rb
@@ -0,0 +1,11 @@
+require 'rest-client'
+url = "http://localhost:3000/users"
+puts RestClient.get(url)
+url = "http://localhost:3000/users/1"
+puts RestClient.get(url)
+url = "http://localhost:3000/users/1/edit"
+puts RestClient.get(url)
+url = "http://localhost:3000/users/new"
+puts RestClient.get(url)
+url = "http://localhost:3000/users/"
+RestClient.post url, :param1 => ""
